74ab75ebfb96299e70f735df797ccf60e8801a6f
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-04-21 19:33:45 +0600

    fix

--- src/api/base.js
+++ src/api/base.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import Cookies from "universal-cookie";
 // import { API_BASE_URL } from "../index";
 // const cookies = new Cookies();
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://mapquest.onrender.com/api";
 
 // axios.defaults.withCredentials = true;
 // axios.interceptors.request.use(
