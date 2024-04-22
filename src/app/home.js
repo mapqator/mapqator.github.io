3c4bfa67a780f52e18abad4e4d320d4d6457d46f
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-04-22 15:05:09 +0600

    credit

--- src/app/home.js
+++ src/app/home.js
@@ -3,9 +3,15 @@ import ContextGenerator from "./ContextGenerator";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-row gap-1 bg-black">
-      <DatasetCreator />
-      <ContextGenerator />
+    <main className="flex min-h-screen flex-col gap-1 bg-black">
+      <div className="flex flex-row">
+        <DatasetCreator />
+        <ContextGenerator />
+      </div>
+
+      <footer className="w-full h-20 text-white flex items-center justify-center">
+        © Mahir Labib Dihan
+      </footer>
     </main>
   );
 }
