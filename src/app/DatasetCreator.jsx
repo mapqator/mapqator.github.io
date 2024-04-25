4ca04613bc5a0d3fbae223d16895334a9affb8b1
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-04-25 20:35:42 +0600

    minor

--- src/app/DatasetCreator.jsx
+++ src/app/DatasetCreator.jsx
@@ -49,252 +49,271 @@ export default function DatasetCreator({ contextJSON, context }) {
   }, []);
   return (
     <div className="w-1/2 flex flex-col items-center p-5 bg-white gap-2">
-      <h1 className="text-3xl">Map Dataset Creator</h1>
+      <div className="bg-white flex flex-col items-center w-full">
+        <h1 className="text-3xl">Map Dataset Creator</h1>
+        <p className="text-lg">
+          Write question and answers with appropriate contexts
+        </p>
+        <h1 className="bg-black h-1 w-full my-2"></h1>
+      </div>
 
-      <h1 className="bg-black h-1 w-full my-2"></h1>
-      <label className="text-lg w-full text-left font-bold">Question</label>
-      <textarea
-        className="border border-black w-full"
-        value={query.question}
-        onChange={(e) =>
-          setQuery((prev) => ({ ...prev, question: e.target.value }))
-        }
-      />
-
-      <FormControl fullWidth>
-        <FormLabel id="demo-radio-buttons-group-label">
-          <label className="text-lg w-full text-left font-bold text-black focus:text-black">
-            Type
-          </label>
-        </FormLabel>
-        <RadioGroup
-          aria-labelledby="demo-radio-buttons-group-label"
-          defaultValue="mcq"
-          name="radio-buttons-group"
-          value={query.answer.type}
-          onChange={(e) =>
-            setQuery((prev) => ({
-              ...prev,
-              answer: {
-                ...prev.answer,
-                type: e.target.value,
-                correct: e.target.value === "mcq" ? -1 : "",
-                options: e.target.value === "mcq" ? [] : undefined,
-              },
-            }))
-          }
-          row
-        >
-          <FormControlLabel value="mcq" control={<Radio />} label="MCQ" />
-          <FormControlLabel value="short" control={<Radio />} label="Short" />
-          {/* <FormControlLabel value="other" control={<Radio />} label="Other" /> */}
-        </RadioGroup>
-      </FormControl>
-
-      {query.answer.type === "mcq" ? (
-        <div className="w-full flex flex-col gap-2">
-          <FormLabel id="demo-radio-buttons-group-label">
-            <label className="text-lg w-full text-left font-bold text-black focus:text-black">
-              Answer
-            </label>
-          </FormLabel>
-
-          <div className="flex flex-row justify-start items-center gap-2">
-            <input
-              type="text"
-              placeholder="Option"
-              className="border border-black rounded p-2"
-              value={newOption}
-              onChange={(e) => setNewOption(e.target.value)}
-            />
-            <button
-              className="bg-blue-500 rounded-lg p-3"
+      <div className="w-full flex flex-col items-center bg-white border-4 rounded-lg border-black">
+        <div className="flex flex-col bg-black w-full items-center p-1">
+          <h1 className="text-3xl text-white">Create new query</h1>
+          <p className="text-base text-white">Question+Context+Answer</p>
+        </div>
+        <div className="p-1 flex flex-col w-full gap-1">
+          <label className="text-lg w-full text-left font-bold">Question</label>
+          <textarea
+            className="border border-black w-full rounded-sm"
+            value={query.question}
+            onChange={(e) =>
+              setQuery((prev) => ({ ...prev, question: e.target.value }))
+            }
+          />
+          <div className="flex flex-row gap-2 w-full justify-start items-center">
+            <label className="text-lg text-left font-bold">Context</label>
+            <Button
+              className="bg-green-500 rounded-lg p-2"
               onClick={() => {
-                if (newOption === "") return;
+                setContextCopied(true);
+                setQuery((prev) => ({
+                  ...prev,
+                  context: context.reduce((acc, e) => acc + e + "\n", ""),
+                  contextJSON: contextJSON,
+                }));
+              }}
+              variant="contained"
+              color="success"
+              disabled={Object.keys(contextJSON).length === 0}
+            >
+              {Object.keys(contextJSON).length === 0
+                ? "Generate context first"
+                : "Use generated context"}
+            </Button>
+          </div>
+          {contextCopied && (
+            <>
+              <textarea
+                className="border border-black w-full"
+                value={query.context}
+                onChange={(e) =>
+                  setQuery((prev) => ({ ...prev, context: e.target.value }))
+                }
+              />
+              <textarea
+                className="border border-black w-full"
+                value={query.contextGPT}
+                onChange={(e) =>
+                  setQuery((prev) => ({ ...prev, contextGPT: e.target.value }))
+                }
+              />
+              <Button
+                className="bg-blue-500 rounded-lg p-2 mt-2 w-full"
+                variant="contained"
+                onClick={async () => {
+                  console.log(query.context);
+                  const res = await queryApi.getGPTContext(query.context);
+                  if (res.success) {
+                    console.log("Context generated successfully");
+                    setQuery((prev) => ({
+                      ...prev,
+                      contextGPT: res.data,
+                    }));
+                  } else {
+                    console.error("Error generating context");
+                  }
+                }}
+              >
+                Generate GPT Context
+              </Button>
+            </>
+          )}
+          <FormControl fullWidth>
+            <FormLabel id="demo-radio-buttons-group-label">
+              <label className="text-lg w-full text-left font-bold text-black focus:text-black">
+                Type
+              </label>
+            </FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="mcq"
+              name="radio-buttons-group"
+              value={query.answer.type}
+              onChange={(e) =>
                 setQuery((prev) => ({
                   ...prev,
                   answer: {
                     ...prev.answer,
-                    options: [...prev.answer.options, newOption],
+                    type: e.target.value,
+                    correct: e.target.value === "mcq" ? -1 : "",
+                    options: e.target.value === "mcq" ? [] : undefined,
                   },
-                }));
-                setNewOption("");
-              }}
+                }))
+              }
+              row
             >
-              + Add option
-            </button>
-          </div>
-          <div className="flex flex-col gap-2">
-            {query.answer.options.map((option, index) => (
-              <div
-                key={index}
-                className="flex flex-row justify-start items-center gap-2"
-              >
+              <FormControlLabel value="mcq" control={<Radio />} label="MCQ" />
+              <FormControlLabel
+                value="short"
+                control={<Radio />}
+                label="Short"
+              />
+              {/* <FormControlLabel value="other" control={<Radio />} label="Other" /> */}
+            </RadioGroup>
+          </FormControl>
+          {query.answer.type === "mcq" ? (
+            <div className="w-full flex flex-col gap-2">
+              <FormLabel id="demo-radio-buttons-group-label">
+                <label className="text-lg w-full text-left font-bold text-black focus:text-black">
+                  Answer
+                </label>
+              </FormLabel>
+
+              <div className="flex flex-row justify-start items-center gap-2">
                 <input
-                  type="radio"
-                  checked={query.answer.correct === index}
-                  onClick={() =>
+                  type="text"
+                  placeholder="Option"
+                  className="border border-black rounded p-2"
+                  value={newOption}
+                  onChange={(e) => setNewOption(e.target.value)}
+                />
+                <button
+                  className="bg-blue-500 rounded-lg p-3"
+                  onClick={() => {
+                    if (newOption === "") return;
                     setQuery((prev) => ({
                       ...prev,
                       answer: {
                         ...prev.answer,
-                        correct: prev.answer.correct === index ? -1 : index,
+                        options: [...prev.answer.options, newOption],
                       },
-                    }))
-                  }
-                />
-                <input
-                  type="text"
-                  className="text-lg"
-                  value={option}
-                  onChange={(e) => {
-                    setQuery((prev) => {
-                      const options = [...prev.answer.options];
-                      options[index] = e.target.value;
-                      return {
-                        ...prev,
-                        answer: { ...prev.answer, options },
-                      };
-                    });
+                    }));
+                    setNewOption("");
                   }}
-                />
-                <IconButton
-                  onClick={() => {
-                    setQuery((prev) => {
-                      const options = [...prev.answer.options];
-                      options.splice(index, 1);
-                      return {
-                        ...prev,
-                        answer: { ...prev.answer, options },
-                      };
-                    });
-                  }}
-                  sx={{ height: "2rem", width: "2rem" }}
                 >
-                  <FontAwesomeIcon icon={faTrash} color="red" size="sm" />
-                </IconButton>
+                  + Add option
+                </button>
               </div>
-            ))}
+              <div className="flex flex-col gap-2">
+                {query.answer.options.map((option, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-row justify-start items-center gap-2"
+                  >
+                    <input
+                      type="radio"
+                      checked={query.answer.correct === index}
+                      onClick={() =>
+                        setQuery((prev) => ({
+                          ...prev,
+                          answer: {
+                            ...prev.answer,
+                            correct: prev.answer.correct === index ? -1 : index,
+                          },
+                        }))
+                      }
+                    />
+                    <input
+                      type="text"
+                      className="text-lg"
+                      value={option}
+                      onChange={(e) => {
+                        setQuery((prev) => {
+                          const options = [...prev.answer.options];
+                          options[index] = e.target.value;
+                          return {
+                            ...prev,
+                            answer: { ...prev.answer, options },
+                          };
+                        });
+                      }}
+                    />
+                    <IconButton
+                      onClick={() => {
+                        setQuery((prev) => {
+                          const options = [...prev.answer.options];
+                          options.splice(index, 1);
+                          return {
+                            ...prev,
+                            answer: { ...prev.answer, options },
+                          };
+                        });
+                      }}
+                      sx={{ height: "2rem", width: "2rem" }}
+                    >
+                      <FontAwesomeIcon icon={faTrash} color="red" size="sm" />
+                    </IconButton>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <>
+              <label className="text-lg w-full text-left font-bold">
+                Answer
+              </label>
+              <textarea
+                className="border border-black w-full"
+                value={query.answer.correct}
+                onChange={(e) =>
+                  setQuery((prev) => ({
+                    ...prev,
+                    answer: { ...prev.answer, correct: e.target.value },
+                  }))
+                }
+              />
+            </>
+          )}
+          <div className="flex flex-col gap-2 mr-auto">
+            <label className="text-lg text-left font-bold">Category</label>
+            <TextField
+              type="text"
+              className="border border-black mr-auto"
+              value={query.class}
+              onChange={(e) =>
+                setQuery((prev) => ({ ...prev, class: e.target.value }))
+              }
+              size="small"
+              // label="Category"
+            />
+          </div>
+          <div className="mx-auto w-[10rem] font-bold">
+            <Button
+              onClick={async () => {
+                const res = await queryApi.createQuery(query);
+                if (res.success) {
+                  console.log("Query saved successfully");
+                  fetchQueries();
+                  setQuery({
+                    question: "",
+                    answer: {
+                      type: "mcq",
+                      options: [],
+                      correct: -1,
+                    },
+                    context: "",
+                    contextJSON: {},
+                    class: "",
+                    contextGPT: "",
+                  });
+                }
+              }}
+              variant="contained"
+              fullWidth
+              sx={{ fontWeight: "bold", fontSize: "1.2rem" }}
+            >
+              Save
+            </Button>
           </div>
         </div>
-      ) : (
-        <>
-          <label className="text-lg w-full text-left font-bold">Answer</label>
-          <textarea
-            className="border border-black w-full"
-            value={query.answer.correct}
-            onChange={(e) =>
-              setQuery((prev) => ({
-                ...prev,
-                answer: { ...prev.answer, correct: e.target.value },
-              }))
-            }
-          />
-        </>
-      )}
-
-      <div className="flex flex-row gap-2 w-full justify-start items-center">
-        <label className="text-lg text-left font-bold">Context</label>
-        <Button
-          className="bg-green-500 rounded-lg p-2"
-          onClick={() => {
-            setContextCopied(true);
-            setQuery((prev) => ({
-              ...prev,
-              context: context.reduce((acc, e) => acc + e + "\n", ""),
-              contextJSON: contextJSON,
-            }));
-          }}
-          variant="contained"
-          color="success"
-          disabled={Object.keys(contextJSON).length === 0}
-        >
-          {Object.keys(contextJSON).length === 0
-            ? "Generate context first"
-            : "Use generated context"}
-        </Button>
       </div>
 
-      {contextCopied && (
-        <>
-          <textarea
-            className="border border-black w-full"
-            value={query.context}
-            onChange={(e) =>
-              setQuery((prev) => ({ ...prev, context: e.target.value }))
-            }
-          />
-          <textarea
-            className="border border-black w-full"
-            value={query.contextGPT}
-            onChange={(e) =>
-              setQuery((prev) => ({ ...prev, contextGPT: e.target.value }))
-            }
-          />
-          <Button
-            className="bg-blue-500 rounded-lg p-2 mt-2 w-full"
-            variant="contained"
-            onClick={async () => {
-              console.log(query.context);
-              const res = await queryApi.getGPTContext(query.context);
-              if (res.success) {
-                console.log("Context generated successfully");
-                setQuery((prev) => ({
-                  ...prev,
-                  contextGPT: res.data,
-                }));
-              } else {
-                console.error("Error generating context");
-              }
-            }}
-          >
-            Generate GPT Context
-          </Button>
-        </>
-      )}
-
-      <div className="flex flex-col gap-2 mr-auto">
-        <label className="text-lg text-left font-bold">Category</label>
-        <TextField
-          type="text"
-          className="border border-black mr-auto"
-          value={query.class}
-          onChange={(e) =>
-            setQuery((prev) => ({ ...prev, class: e.target.value }))
-          }
-          size="small"
-          // label="Category"
-        />
-      </div>
-
-      <button
-        className="bg-blue-500 rounded-lg p-2 mt-2 w-full"
-        onClick={async () => {
-          const res = await queryApi.createQuery(query);
-          if (res.success) {
-            console.log("Query saved successfully");
-            fetchQueries();
-            setQuery({
-              question: "",
-              answer: {
-                type: "mcq",
-                options: [],
-                correct: -1,
-              },
-              context: "",
-              contextJSON: {},
-              class: "",
-            });
-          }
-        }}
-      >
-        Save
-      </button>
-      <h1 className="bg-black h-1 w-full my-2"></h1>
-      <h1 className="text-2xl w-full">Previous Queries</h1>
+      {/*<h1 className="bg-black h-1 w-full my-2"></h1>*/}
+      {/*<h1 className="text-2xl w-full">Previous Queries</h1>*/}
       <div className="flex flex-col gap-2 mt-1">
         {queries.map((query, index) => (
-          <QueryCard key={index} query={query} />
+          <QueryCard key={index} query={query} index={index} />
         ))}
       </div>
     </div>
--- src/app/QueryCard.jsx
+++ src/app/QueryCard.jsx
@@ -10,31 +10,37 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import QueryApi from "@/api/queryApi";
 const queryApi = new QueryApi();
-export default function QueryCard({ query }) {
+export default function QueryCard({ query, index }) {
   const [expanded, setExpanded] = useState(false);
   return (
-    <div>
-      {expanded ? (
-        <div className="flex flex-col gap-1 p-1 border-2 border-black rounded-md">
-          <div className="flex flex-row items-end justify-between">
-            <h1 className="text-xl font-bold">Question</h1>
-            <div>
-              <IconButton
-                sx={{ height: "3rem", width: "3rem" }}
-                onClick={() => setExpanded(false)}
-              >
-                <div className="text-sm md:text-2xl">
-                  <FontAwesomeIcon
-                    icon={expanded ? faChevronUp : faChevronDown}
-                    color="black"
-                  />
-                </div>
-              </IconButton>
+    <div className="flex flex-col border-4 border-black rounded-md">
+      <div className="flex flex-row items-center justify-between bg-black">
+        <h1 className="text-2xl font-bold text-white pl-2"> # {index + 1}</h1>
+        <div>
+          <IconButton
+            sx={{ height: "3rem", width: "3rem" }}
+            onClick={() => setExpanded((prev) => !prev)}
+          >
+            <div className="text-sm md:text-2xl">
+              <FontAwesomeIcon
+                icon={expanded ? faChevronUp : faChevronDown}
+                color="white"
+              />
             </div>
-          </div>
+          </IconButton>
+        </div>
+      </div>
+      {expanded ? (
+        <div className="px-1 pb-1">
+          <h1 className="text-lg font-bold underline">Context (Template)</h1>
+          <h1 className="text-lg">{query.context}</h1>
+          <h1 className="text-lg font-bold underline">Context (GPT)</h1>
+          <h1 className="text-lg">{query.context_gpt}</h1>
+
+          <h1 className="text-xl font-bold underline">Question</h1>
           <h1 className="text-lg">{query.question}</h1>
 
-          <h1 className="text-lg font-bold">Answer</h1>
+          <h1 className="text-lg font-bold underline">Answer</h1>
           {query.answer.type === "mcq" ? (
             <div className="flex flex-col gap-1">
               {query.answer.options.map((option, index) => (
@@ -50,8 +56,7 @@ export default function QueryCard({ query }) {
           ) : (
             <h1 className="text-lg">{query.answer.correct}</h1>
           )}
-          <h1 className="text-lg font-bold">Context</h1>
-          <h1 className="text-lg">{query.context}</h1>
+
           <div className="flex flex-row gap-2 mx-auto">
             <Button
               variant="contained"
@@ -63,10 +68,7 @@ export default function QueryCard({ query }) {
                 );
               }}
             >
-              Ask Gemini
-            </Button>
-            <Button variant="contained" color="primary">
-              Ask GPT
+              Copy
             </Button>
             <Button
               variant="contained"
@@ -85,22 +87,7 @@ export default function QueryCard({ query }) {
           </div>
         </div>
       ) : (
-        <div className="flex flex-row gap-1 border-2 border-black items-center p-1 rounded-md">
-          <h1 className="text-lg w-[95%]">{query.question}</h1>
-          <div>
-            <IconButton
-              sx={{ height: "3rem", width: "3rem" }}
-              onClick={() => setExpanded(true)}
-            >
-              <div className="text-sm md:text-2xl">
-                <FontAwesomeIcon
-                  icon={expanded ? faChevronUp : faChevronDown}
-                  color="black"
-                />
-              </div>
-            </IconButton>
-          </div>
-        </div>
+        <h1 className="text-lg px-1">{query.question}</h1>
       )}
     </div>
   );
