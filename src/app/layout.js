a5a38124dcdde91649e921544c5b44ae61906633
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-06-20 15:56:05 +0600

    metaBase added

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,14 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+	metadataBase: new URL("https://mahirlabibdihan.github.io/mapquest"),
 	title: "MapQuest",
 	description: "Map dataset creator",
+	// openGraph: {
+	// 	title: "Title webtsite",
+	// 	description: "this is the desciption",
+	// 	image: "url/image.png",
+	// },
 };
 
 export default function RootLayout({ children }) {
