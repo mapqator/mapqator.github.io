e1f83bd3528a174a3b207d5b9ee048ab77fbcc8e
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-06-26 17:02:17 +0600

    check

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -5,6 +5,7 @@ import Api, { API_BASE_URL } from "./base";
 class AuthApi extends Api {
 	login = async (data) => {
 		try {
+			console.log("POST: ", (API_BASE_URL + "/auth/login", data));
 			let res = await axios.post(API_BASE_URL + "/auth/login", data);
 			return {
 				success: true,
