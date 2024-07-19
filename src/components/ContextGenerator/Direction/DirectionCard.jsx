9205ddeb1835fb1188157dd6271b74a47cf92029
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-07-20 02:17:51 +0600

    Direction Card Complete

--- src/components/ContextGenerator/Direction/DirectionCard.jsx
+++ src/components/ContextGenerator/Direction/DirectionCard.jsx
@@ -5,6 +5,7 @@ import {
 	Box,
 	Card,
 	CardContent,
+	Checkbox,
 	Chip,
 	Collapse,
 	Divider,
@@ -13,6 +14,8 @@ import {
 	ListItem,
 	ListItemIcon,
 	ListItemText,
+	Paper,
+	Switch,
 	Typography,
 } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -30,6 +33,12 @@ import { Delete, ExpandMore } from "@mui/icons-material";
 import { GlobalContext } from "@/contexts/GlobalContext";
 export default function DirectionCard({ from_id, to_id }) {
 	const [expanded, setExpanded] = useState(false);
+	const [expandedRoute, setExpandedRoute] = useState({
+		walking: false,
+		driving: false,
+		bicycling: false,
+		transit: false,
+	});
 	const {
 		directionInformation,
 		setDirectionInformation,
@@ -118,29 +127,194 @@ export default function DirectionCard({ from_id, to_id }) {
 											<FontAwesomeIcon icon={faBus} />
 										)}
 									</ListItemIcon>
-									<ListItemText
-										primary={
-											mode === "walking"
-												? "On foot"
-												: mode === "driving"
-												? "By car"
-												: mode === "bicycling"
-												? "By cycle"
-												: "By public transport"
-										}
-										secondary={
-											directionInformation[from_id][
-												to_id
-											][mode].routes.length + " routes"
-										}
-										primaryTypographyProps={{
-											noWrap: true,
-										}}
-										secondaryTypographyProps={{
-											noWrap: true,
-										}}
-									/>
+									<div className="flex flex-row gap-2 items-center w-full">
+										<div className="w-1/2">
+											<ListItemText
+												primary={
+													mode === "walking"
+														? "On foot"
+														: mode === "driving"
+														? "By car"
+														: mode === "bicycling"
+														? "By cycle"
+														: "By public transport"
+												}
+												secondary={
+													directionInformation[
+														from_id
+													][to_id][mode].routes
+														.length + " routes"
+												}
+												primaryTypographyProps={{
+													noWrap: true,
+												}}
+												secondaryTypographyProps={{
+													noWrap: true,
+												}}
+											/>
+										</div>
+										{/* <Checkbox
+											edge="start"
+											checked={
+												directionInformation[from_id][
+													to_id
+												][mode].showSteps
+											}
+											onChange={() => {
+												const newDirectionMatrix = {
+													...directionInformation,
+												};
+												newDirectionMatrix[from_id][
+													to_id
+												][mode].showSteps =
+													!newDirectionMatrix[
+														from_id
+													][to_id][mode].showSteps;
+												setDirectionInformation(
+													newDirectionMatrix
+												);
+											}}
+										/> */}
+										<div className="ml-auto">
+											<IconButton
+												onClick={() =>
+													setExpandedRoute(
+														(prev) => ({
+															...prev,
+															[mode]: !prev[mode],
+														})
+													)
+												}
+												size="small"
+												sx={{
+													transform: expandedRoute[
+														mode
+													]
+														? "rotate(180deg)"
+														: "rotate(0deg)",
+													transition: "0.3s",
+												}}
+											>
+												<ExpandMore />
+											</IconButton>
+										</div>
+									</div>
 								</ListItem>
+								<Collapse
+									in={expandedRoute[mode]}
+									timeout="auto"
+									unmountOnExit
+								>
+									{/* <Divider /> */}
+									<Paper
+										elevation={1}
+										sx={{
+											bgcolor: "grey.100",
+											m: 2,
+											mt: 0,
+										}}
+									>
+										<Box className="w-full p-3 pb-0 flex flex-row justify-end items-center">
+											<h6 className="text-xs text-zinc-500">
+												Show Steps
+											</h6>
+											<Switch
+												onChange={() => {
+													const newDirectionMatrix = {
+														...directionInformation,
+													};
+													newDirectionMatrix[from_id][
+														to_id
+													][mode].showSteps =
+														!newDirectionMatrix[
+															from_id
+														][to_id][mode]
+															.showSteps;
+													setDirectionInformation(
+														newDirectionMatrix
+													);
+												}}
+												checked={
+													directionInformation[
+														from_id
+													][to_id][mode].showSteps
+												}
+												size="small"
+											/>
+										</Box>
+										<List dense>
+											{directionInformation[from_id][
+												to_id
+											][mode].routes.map(
+												(route, index) => (
+													<React.Fragment key={index}>
+														<Divider/>
+														<ListItem>
+															<div className="flex flex-col gap-2 w-full">
+																<ListItemText
+																	primary={
+																		<div className="flex flex-row justify-between w-full">
+																			<h1 className="w-[70%]">
+																				{
+																					route.label
+																				}
+																			</h1>
+																			<h1 className="text-md text-right w-[15%]">
+																				{
+																					route.duration
+																				}
+																			</h1>
+																			<h1 className="text-md text-right w-[15%]">
+																				{
+																					route.distance
+																				}
+																			</h1>
+																		</div>
+																	}
+																	secondary={
+																		directionInformation[
+																			from_id
+																		][
+																			to_id
+																		][mode]
+																			.showSteps && (
+																			<div className="flex flex-col gap-0">
+																				{route.steps.map(
+																					(
+																						step,
+																						index1
+																					) => (
+																						<p
+																							key={
+																								index1
+																							}
+																							// className="text-sm"
+																							dangerouslySetInnerHTML={{
+																								__html:
+																									"- " +
+																									step,
+																							}}
+																						/>
+																					)
+																				)}
+																			</div>
+																		)
+																	}
+																	primaryTypographyProps={{
+																		noWrap: true,
+																	}}
+																	secondaryTypographyProps={{
+																		noWrap: true,
+																	}}
+																/>
+															</div>
+														</ListItem>
+													</React.Fragment>
+												)
+											)}
+										</List>
+									</Paper>
+								</Collapse>
 								{index3 <
 									Object.keys(
 										directionInformation[from_id][to_id]
