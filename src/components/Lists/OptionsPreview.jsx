fbbb53f05d8a2504b5b78ca2cd8af5af83844b9e
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-07-23 19:31:55 +0600

    code reorganize

--- src/components/Lists/OptionsPreview.jsx
+++ src/components/Lists/OptionsPreview.jsx
@@ -1,44 +1,14 @@
 "use client";
 import React, { useState, useEffect, useContext } from "react";
 import {
-	Container,
 	Typography,
-	Paper,
-	Accordion,
-	AccordionSummary,
-	AccordionDetails,
 	Box,
 	Chip,
 	List,
 	ListItem,
 	ListItemText,
-	Select,
-	MenuItem,
-	FormControl,
-	InputLabel,
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead,
-	TableRow,
-	Button,
-	Collapse,
-	OutlinedInput,
-	TextField,
-	Pagination,
 } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { GlobalContext } from "@/contexts/GlobalContext";
-import QueryApi from "@/api/queryApi";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { Clear, Edit, Save } from "@mui/icons-material";
-import { useRouter } from "next/navigation";
-import MapApi from "@/api/mapApi";
 
-const queryApi = new QueryApi();
-const mapApi = new MapApi();
 export default function OptionsPreview({ answer }) {
 	useEffect(() => {
 		console.log(answer);
