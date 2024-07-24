4d1dd36ba7a84617895c8f0ed4d63750741c382b
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-07-24 17:17:09 +0600

    Routeslist separated

--- src/components/Cards/DirectionCard.jsx
+++ src/components/Cards/DirectionCard.jsx
@@ -24,57 +24,8 @@ import {
 	convertTravelModeToIcon,
 	convertTravelModeToLabel,
 } from "@/services/utils";
+import RoutesList from "../Lists/RoutesList";
 
-function RoutesList({ routes, showSteps }) {
-	return (
-		<List dense>
-			{routes.map((route, index) => (
-				<React.Fragment key={index}>
-					<Divider />
-					<ListItem>
-						<div className="flex flex-col gap-2 w-full">
-							<ListItemText
-								primary={
-									<div className="flex flex-row justify-between w-full">
-										<h1 className="w-[60%] text-wrap">
-											{"Via " + route.label}
-										</h1>
-										<h1 className="w-[40%] text-right">
-											{route.duration +
-												" | " +
-												route.distance}
-										</h1>
-									</div>
-								}
-								secondary={
-									showSteps && (
-										<div className="flex flex-col gap-0">
-											{route.steps.map((step, index1) => (
-												<p
-													key={index1}
-													// className="text-sm"
-													dangerouslySetInnerHTML={{
-														__html: "- " + step,
-													}}
-												/>
-											))}
-										</div>
-									)
-								}
-								primaryTypographyProps={{
-									noWrap: true,
-								}}
-								secondaryTypographyProps={{
-									noWrap: true,
-								}}
-							/>
-						</div>
-					</ListItem>
-				</React.Fragment>
-			))}
-		</List>
-	);
-}
 function DirectionCardDetails({ from_id, to_id }) {
 	const [expandedRoute, setExpandedRoute] = useState({
 		walking: false,
