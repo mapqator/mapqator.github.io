6996fa2ddb41bb6e1568bcad85f1a6fe3cbdb808
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-07-24 17:20:50 +0600

    code clean

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
 import GlobalContextProvider from "@/contexts/GlobalContext";
 import Home from "./page/Home";
 
 export default function PageComponent() {
-	const [activeStep, setActiveStep] = useState(null);
-	const [selected, setSelected] = useState("context");
-
-	useEffect(() => {
-		const page = window.location.hash.substring(1);
-		setSelected(page === "" || page === "onboard" ? "context" : page);
-	}, []);
-
-	useEffect(() => {
-		if (selected === "context" && activeStep === null) {
-			setActiveStep(
-				window.location.hash.substring(1) === "onboard" ? 0 : 1
-			);
-		}
-	}, [selected]);
-
 	return (
 		<GlobalContextProvider>
 			<Home />
