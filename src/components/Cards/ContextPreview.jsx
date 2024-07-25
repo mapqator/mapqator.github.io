c962d762e97502721608689c5d0372d465739506
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-07-25 18:32:19 +0600

    Context collapsed

--- src/components/Cards/ContextPreview.jsx
+++ src/components/Cards/ContextPreview.jsx
@@ -1,22 +1,51 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 export default function ContextPreview({ context }) {
+	const [contextExpanded, setContextExpanded] = useState(false);
+
 	return (
-		<div className="p-0 md:p-2 flex flex-col gap-2 w-full min-h-16">
-			{context?.length > 0 ? (
-				<p
-					className="w-full text-left text-sm"
-					style={{
-						whiteSpace: "pre-line",
-					}}
-					dangerouslySetInnerHTML={{
-						__html: context,
-					}}
-				/>
-			) : (
-				<p className="text-center my-auto text-lg md:text-xl text-zinc-400">
+		<div
+			onClick={() => setContextExpanded((prev) => !prev)}
+			className="cursor-pointer"
+		>
+			{context.split("\n").map(
+				(line, index) =>
+					(contextExpanded || index < 5) && (
+						<p
+							key={index}
+							className="w-full text-left"
+							style={{
+								whiteSpace: "pre-line",
+							}}
+							dangerouslySetInnerHTML={{
+								__html:
+									line +
+									(line === "" &&
+									index < context.split("\n").length - 1
+										? "\n"
+										: ""),
+							}}
+						/>
+					)
+			)}
+
+			{context === "" && (
+				<p className="text-center py-2 my-auto text-lg md:text-xl text-zinc-400">
 					No context generated. Add information first.
 				</p>
+			)}
+
+			{context.split("\n").length > 5 && (
+				<Button sx={{ mt: 1, textTransform: "none" }}>
+					<div className="flex flex-row gap-1 items-end">
+						{!contextExpanded && (
+							<FontAwesomeIcon icon={faEllipsis} />
+						)}
+						{contextExpanded ? "Show Less" : "Read More"}
+					</div>
+				</Button>
 			)}
 		</div>
 	);
