ba7fc9977bf396a296bfe7fb2bdbaff3cefdb3dd
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-07-26 02:49:21 +0600

    api key field type password

--- src/components/Buttons/KeyStoreButton.jsx
+++ src/components/Buttons/KeyStoreButton.jsx
@@ -3,7 +3,14 @@
 import { faGear, faGears } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Add, Settings } from "@mui/icons-material";
-import { Box, Button, CardContent, Dialog, DialogTitle } from "@mui/material";
+import {
+	Box,
+	Button,
+	CardContent,
+	Dialog,
+	DialogTitle,
+	InputAdornment,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import CloseIcon from "@mui/icons-material/Close";
@@ -12,10 +19,13 @@ import TextField from "@mui/material/TextField";
 import LoadingButton from "@mui/lab/LoadingButton";
 import Skeleton from "@mui/material/Skeleton";
 import { showSuccess } from "@/contexts/ToastProvider";
+import MuiPasswordField from "../InputFields/PasswordField";
+import EyeIcon from "../Icons/EyeIcon";
 
 export default function KeyStoreButton({ onClick }) {
 	const [open, setOpen] = useState(false);
 	const [key, setKey] = useState("");
+	const [showKey, setShowKey] = useState(false);
 	const handleClose = () => {
 		setOpen(false);
 	};
@@ -41,11 +51,23 @@ export default function KeyStoreButton({ onClick }) {
 				<CardContent>
 					<Box className="flex flex-col gap-4">
 						<TextField
+							required
 							label="Google Map Api Key"
-							variant="outlined"
-							className="w-full"
+							type={showKey ? "text" : "password"}
 							value={key}
 							onChange={(e) => setKey(e.target.value)}
+							fullWidth
+							InputProps={{
+								endAdornment: (
+									<InputAdornment position="end">
+										<EyeIcon
+											isVisible={key.length > 0}
+											showPassword={showKey}
+											setShowPassword={setShowKey}
+										/>
+									</InputAdornment>
+								),
+							}}
 						/>
 						<Button
 							onClick={handleSave}
