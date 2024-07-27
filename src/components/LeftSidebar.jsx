4e8f126c95266a6412e7a893debd1074e06d9a91
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-07-28 03:49:41 +0600

    center

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -128,7 +128,7 @@ export default function LeftSidebar({ selected, setSelected, window }) {
 		<div>
 			<Toolbar className="!px-4">
 				<div
-					className="text-3xl font-bold flex flex-row justify-center w-full gap-2 cursor-pointer"
+					className="text-3xl font-bold flex flex-row justify-center items-center w-full gap-2 cursor-pointer"
 					onClick={() => router.push("/landing")}
 				>
 					<div className="hidden lg:flex">
