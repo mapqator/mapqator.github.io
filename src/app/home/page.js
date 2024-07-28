4e4deaf61b1c21e983386ae7f91c9a2cb5e4568c
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-07-28 18:27:58 +0600

    minor

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -105,7 +105,7 @@ export default function Home() {
 	];
 
 	return (
-		<div className="h-full flex md:items-center p-2 pt-10 md:pt-2">
+		<div className="h-full flex md:items-center justify-center p-2 pt-5 md:pt-2">
 			<div className="flex flex-col md:flex-row items-center mx-auto">
 				{steps.map((step, index) => (
 					<div
